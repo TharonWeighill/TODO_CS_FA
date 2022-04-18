7aa2616609802239d8adb46f0ab0c281dbd7a990
tharonweighill <tharon@fusionauth.io>
2022-04-18 08:13:09 -0600

    Create User Model

---
+++ src/main/java/com/tharon/todo/Models/User.java
@@ -0,0 +1,11 @@
+package com.tharon.todo.Models;
+
+
+public class User {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String favorite_instrument;
+}
