98a0570cdc0c210da490aa726421787b34903628
tharonweighill <tharon@fusionauth.io>
2022-04-18 08:25:00 -0600

    Generate Getter and Setter User Model

--- src/main/java/com/tharon/todo/Models/User.java
+++ src/main/java/com/tharon/todo/Models/User.java
@@ -8,4 +8,45 @@ public class User {
     private String lastName;
     private String email;
     private String favorite_instrument;
+
+    public void setFavorite_instrument(String favorite_instrument) {
+        this.favorite_instrument = favorite_instrument;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFavorite_instrument() {
+        return favorite_instrument;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
+
