2965469fca43548c31606551ec62df1d1fd68625
tharonweighill <tharon@fusionauth.io>
2022-04-19 11:17:38 -0600

    create ToDo Class

---
+++ src/main/java/com/tharon/todo/Models/Todo.java
@@ -0,0 +1,16 @@
+package com.tharon.todo.Models;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String todo;
+
+        }
