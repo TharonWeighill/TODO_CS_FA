90b40108daf40c63ca1fa0e8829903a9a57159b9
tharonweighill <tharon@fusionauth.io>
2022-04-20 14:58:29 -0600

    FIX api controller

--- src/main/java/com/tharon/todo/Models/Todo.java
+++ src/main/java/com/tharon/todo/Models/Todo.java
@@ -1,17 +1,12 @@
 package com.tharon.todo.Models;
-
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 public class Todo {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     @Column
     private String list;
-
 }
