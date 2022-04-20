0d8d3fd389118bbeaddca0ee10375c6d6a4c2995
tharonweighill <tharon@fusionauth.io>
2022-04-20 11:55:37 -0600

    Connect my SQL DB

--- src/main/java/com/example/tharon_todo/TharonTodoApplication.java
+++ src/main/java/com/example/tharon_todo/TharonTodoApplication.java
@@ -1,11 +1,9 @@
 package com.example.tharon_todo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TharonTodoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TharonTodoApplication.class, args);
     }
--- src/main/java/com/tharon/todo/Controller/apiController.java
+++ src/main/java/com/tharon/todo/Controller/apiController.java
@@ -1,12 +1,12 @@
 package com.tharon.todo.Controller;
-
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class apiController {
     @GetMapping(value = "/")
-        public String getPage() {
-        return "Hello World";
+    public String hello(ModelMap Model) {
+        return "list";
     }
 }
